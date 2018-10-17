7f4b11bfe70a40b9d5748740bbe1b3e13658cf8b
zhanglei25 <zhanglei25@le.com>
2018-10-17 17:40:42 +0800

    init

---
+++ vue.config.js
@@ -0,0 +1,75 @@
+module.exports = {
+    // 基本路径
+    baseUrl: process.env.NODE_ENV === 'production' ? '/my-project/' : '/', // 输出文件目录
+    outputDir: 'dist',
+    // eslint-loader 是否在保存的时候检查
+    lintOnSave: true,
+    // webpack配置
+    // see https://github.com/vuejs/vue-cli/blob/dev/docs/webpack.md
+    chainWebpack: () => {},
+    configureWebpack: () => {},
+    // vue-loader 配置项
+    // https://vue-loader.vuejs.org/en/options.html
+    // vueLoader: {},
+    // 生产环境是否生成 sourceMap 文件
+    productionSourceMap: true,
+    // css相关配置
+    css: {
+        // 是否使用css分离插件 ExtractTextPlugin
+        extract: true,
+        // 开启 CSS source maps?
+        sourceMap: false,
+        // css预设器配置项
+        loaderOptions: {},
+        // 启用 CSS modules for all css / pre-processor files.
+        modules: false
+    },
+    // use thread-loader for babel & TS in production build
+    // enabled by default if the machine has more than 1 cores
+    parallel: require('os').cpus().length > 1,
+
+    // webpack-dev-server 相关配置
+    devServer: {
+        open: process.platform === 'darwin',
+        host: '127.0.0.1',
+        port: 8080,
+        https: false,
+        hotOnly: true,
+        proxy: {
+            '/api': {
+                target: 'http://127.0.1.1:8080',
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/api': ''
+                }
+            }
+        },
+        before: app => {}
+    },
+    // 第三方插件配置
+    pluginOptions: {
+        // ...
+    },
+    // 配置多入口
+    pages: {
+        index: {
+            // page 的入口
+            entry: 'src/conf/main.js',
+            // 模板来源
+            template: 'public/index.html',
+            // 在 dist/index.html 的输出
+            filename: 'index.html',
+            // 当使用 title 选项时，
+            // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
+            title: 'Index Page',
+            // 在这个页面中包含的块，默认情况下会包含
+            // 提取出来的通用 chunk 和 vendor chunk。
+            chunks: ['chunk-vendors', 'chunk-common', 'index']
+        },
+        // 当使用只有入口的字符串格式时，
+        // 模板会被推导为 `public/subpage.html`
+        // 并且如果找不到的话，就回退到 `public/index.html`。
+        // 输出文件名会被推导为 `subpage.html`。
+        subpage: 'src/conf/main.js'
+    }
+};
